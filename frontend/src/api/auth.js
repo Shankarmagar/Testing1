f09b5fec1de0da9804c6f2cf650f1cc07b093e21
Shankar Ale Magar <shankaralemagar91@gmail.com>
2023-12-18 14:03:31 -0500

    Deploy

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; // Import axios here
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'http://localhost:3001/';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
